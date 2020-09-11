e031679019d0d225bc4d1de7ebf9538f75915e86
kwj浅浅 <1515862034@qq.com>
2020-09-11 11:45:28 +0800

    1

---
+++ App.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+      <p>hello</p>
+      {{val}}
+  </div>
+</template>
+
+<script>
+import Vue from 'vue'
+import Component from 'vue-class-component'
+
+
+@Component
+export default class App extends Vue {
+    val = 'hhh'
+    mounted(){
+        console.log(this.val)
+    }
+}
+</script>
+
+<style>
+
+</style>
